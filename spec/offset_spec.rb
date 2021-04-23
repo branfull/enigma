fd260436831a24b8bf05f6d00c897e8068cef1c2
Brant Fuller <brant.fuller@gmail.com>
2021-04-22 22:12:35 -0500

    Create Offset#date_squared test

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -10,7 +10,13 @@ RSpec.describe Offset do
   describe '#date_as_integer' do
     offset = Offset.new('040895')
     it 'converts the date from a string to an integer' do
-      expect(offset.date_as_integer).to eq(40895)
+      expect(offset.date_as_integer).to eq(40_895)
+    end
+  end
+  describe '#date_squared' do
+    offset = Offset.new('040895')
+    it 'squares the date' do
+      expect(offset.date_squared).to eq(1_672_401_025)
     end
   end
 end
