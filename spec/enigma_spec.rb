9496705ac79adb2673b77e3468b8bc13f98a1605
Brant Fuller <brant.fuller@gmail.com>
2021-04-24 12:35:29 -0500

    Create Enigma class and test file

---
+++ spec/enigma_spec.rb
@@ -0,0 +1,10 @@
+require './lib/enigma'
+
+RSpec.describe Enigma do
+  enigma = Enigma.new
+  describe '#initialize' do
+    it 'exists' do
+      expect(enigma).to be_an_instance_of(Enigma)
+    end
+  end
+end
